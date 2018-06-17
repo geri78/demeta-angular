d40a7f03aa3243f5cb44a306c32d8756b86cf2b5
GB <gbader@gmx.at>
2018-06-17 08:27:14 +0200

    -

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -11,15 +11,15 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   constructor(private authentication: AuthenticationService, private router: Router, private _web3wrap: Web3WrapperService) { }
-/*
+
   ngOnInit() {
-    this._web3wrap.checkAndInstantiateWeb3();
+/*    this._web3wrap.checkAndInstantiateWeb3();
     this._web3wrap.onReady();
     this._web3wrap.initAxAgroTokenContract();
     this._web3wrap.AxEuroToken_approve( '0x001C6dFbEDfa738570A4020AFB95564dC1027FDC', 13,
-                                        '0x001988b4c0D7Ad7f103D5DB264aB8139A38b8C6a', null);
+                                        '0x001988b4c0D7Ad7f103D5DB264aB8139A38b8C6a', null);*/
 
-    }*/
+    }
 /*
   @HostListener('window:load')
   windowLoaded() {
