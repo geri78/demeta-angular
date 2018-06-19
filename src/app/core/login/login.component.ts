1114c70f24901ee4dc5a19e73cceb526c4296c88
GB <gbader@gmx.at>
2018-06-19 20:37:21 +0200

    Login

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -3,16 +3,31 @@ import { Component, OnInit, HostListener, NgZone  } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
 import { User } from './user';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  public show = false;
+  public buttonName = 'Show';
+  private formSubmitAttempt: boolean;
   _actUser: User;
 
-  constructor(private authentication: AuthenticationService, private router: Router, private _web3wrap: Web3WrapperService) { }
+  // set the default value of our number
+  constructor(private authentication: AuthenticationService,
+    private router: Router,
+    private _web3wrap: Web3WrapperService) { }
 
   ngOnInit() {
     this._web3wrap.checkAndInstantiateWeb3();
@@ -20,8 +35,42 @@ export class LoginComponent implements OnInit {
     this._web3wrap.initAxAgroTokenContract();
    /* this._web3wrap.AxEuroToken_approve( '0x0051D40C97ca3e7581752689ae2F36Fdbb2365bC', 1,
                                         '0x003A9B54a8Baf82F493c9980C031Ec70CcB2D67a', null);*/
+    /*this.form = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]*/
+  }
 
+  get stateName() {
+    return this.show ? 'show' : 'hide';
+  }
+
+
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if (this.show) {
+      this.buttonName = 'Hide';
+    } else {
+      this.buttonName = 'Show';
     }
+  }
+
+  /*isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value); // {7}
+    }
+    this.formSubmitAttempt = true;             // {8}
+  }*/
+
+
 /*
   @HostListener('window:load')
   windowLoaded() {
@@ -42,5 +91,4 @@ export class LoginComponent implements OnInit {
   public getActUser(): User {
     return this._actUser;
   }
-
 }
