2600caf2a647c514c5456462d42f9ed4fd9d2b80
GB <gbader@gmx.at>
2018-06-22 18:25:21 +0200

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
