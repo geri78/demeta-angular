a550bf932776aadd2d0ade4322968d1a7383b458
GB <gbader@gmx.at>
2018-07-13 19:10:01 +0200

    -

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { ActualDSService } from '../services/actualDS.service';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,10 @@ import { AuthenticationService } from '../services/authentication.service';
 export class HeaderComponent implements OnInit {
   /*true - shows header always. False, header is hidden at login*/
   public showHeader_bol = false;
+  public footer_text = '';
   showHeaderSubscription: any;
 
-  constructor(private authentication: AuthenticationService) {
-
-
-
+  constructor(private authentication: AuthenticationService, private _actDSService: ActualDSService) {
   }
 
   ngOnInit() {
@@ -25,10 +24,17 @@ export class HeaderComponent implements OnInit {
 
   showHeader(visible: boolean) {
     this.showHeader_bol = visible;
+    //header showed, than also footer gets showed
+    if (visible = true) {
+      this.setFooterContent();
+    }
+  }
+
+  setFooterContent() {
+    this.footer_text = 'Welcome Gerald Bader and Company : ' + this._actDSService.getCompany().name;
   }
 
   logout() {
     this.authentication.logout();
   }
-
 }
